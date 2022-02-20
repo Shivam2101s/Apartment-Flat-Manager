a8422833e4ec1e189dfc7b5cd5a275869494a7b5
Shivam Sharma <mechbuddy.shivam@gmail.com>
2022-02-20 17:24:54 +0530

    filter and sorting

--- backend/controllers/flat.controller.js
+++ backend/controllers/flat.controller.js
@@ -4,63 +4,17 @@ const Flat = require("../models/flat.model");
 
 const router = express.Router();
 
-router.get("", async (req, res) => {
-  try {
-    const page = +req.query.page || 1;
-    const size = +req.query.size || 4;
-    const skip = (page-1)*size;
-    let flats = await Flat.find().populate("residents").skip(skip).limit(size).lean().exec();
- 
-    
-    const totalPage = Math.ceil((await Flat.find().countDocuments()) / size);
-    return res.status(201).send({flats,totalPage});
-  } catch (e) {
-   return res.status(500).json({ status: "Failed", message: e.message });
-  }
-});
 
 router.post("", async (req, res) => {
   try {
     let flat = await Flat.create(req.body);
 
     return res.status(201).send({ flat });
-
   } catch (e) {
-   return res.status(500).json({ status: "Failed", message: e.message });
+    return res.status(500).json({ status: "Failed", message: e.message });
   }
 });
 
-router.get("/:id", async (req, res) => {
-  try {
-    let flat = await Flat.findById(req.params.id).populate("residents").lean().exec();
-
-    return res.status(201).send( flat );
-  } catch (e) {
-   return res.status(500).json({ status: "Failed", message: e.message });
-  }
-});
-
-router.get("/sort/:value", async (req, res) => {
-  try {
-    const page = +req.query.page || 1;
-    const size = +req.query.size || 4;
-    const skip = (page-1)*size;
-    const totalPage = Math.ceil((await Flat.find().countDocuments()) / size);
-
-    if(req.params.value === "lowToHigh"){
-      let flats = await Flat.find().sort({flat_number:1}).skip(skip).limit(size).populate("residents").lean().exec();
-
-      return res.status(201).send({flats,totalPage} );
-    }else if(req.params.value  === "highToLow"){
-      let flats = await Flat.find().sort({flat_number:-1}).skip(skip).limit(size).populate("residents").lean().exec();
-
-      return res.status(201).send({flats,totalPage} );
-    }
-    
-  } catch (e) {
-   return res.status(500).json({ status: "Failed", message: e.message });
-  }
-});
 
 router.patch("/:id", async (req, res) => {
   try {
@@ -72,11 +26,90 @@ router.patch("/:id", async (req, res) => {
 
     return res.status(201).send({ flat });
   } catch (e) {
-   return res.status(500).json({ status: "Failed", message: e.message });
+    return res.status(500).json({ status: "Failed", message: e.message });
   }
 });
 
+router.get("/:id", async (req, res) => {
+  try {
+    let flat = await Flat.findById(req.params.id)
+      .populate("residents")
+      .lean()
+      .exec();
 
+    return res.status(201).send(flat);
+  } catch (e) {
+    return res.status(500).json({ status: "Failed", message: e.message });
+  }
+});
 
+//-------All data , filter and sorting API-----------
+
+//`http://localhost:4500/flat/${filter}/${value}/?page=${page}&size=${4}`
+
+router.get("/:filter/:sort", async (req, res) => {
+  try {
+    const page = +req.query.page || 1;
+    const size = +req.query.size || 4;
+    const skip = (page - 1) * size;
+    const filter = req.params.filter ;
+    const sort = req.params.sort ;
+
+    if (filter == 0 && sort == 0) {
+      let flats = await Flat.find()
+        .populate("residents")
+        .skip(skip)
+        .limit(size)
+        .lean()
+        .exec();
+
+      let totalPage = Math.ceil((await Flat.find().countDocuments()) / size);
+
+      return res.status(201).send({ flats, totalPage });
+
+    } else if (filter != 0 && sort== 0) {
+      let flats = await Flat.find({ type: filter })
+        .skip(skip)
+        .limit(size)
+        .populate("residents")
+        .lean()
+        .exec();
+
+      let totalPage = Math.ceil(
+        (await Flat.find({ type: filter }).countDocuments()) / size);
+
+      return res.status(201).send({ flats, totalPage });
+    }else if (filter != 0 && sort != 0)  {
+      let flats = await Flat.find({ type: filter })
+        .sort({flat_number:+sort})
+        .skip(skip)
+        .limit(size)
+        .populate("residents")
+        .lean()
+        .exec();
+
+      let totalPage = Math.ceil(
+        (await Flat.find({ type: filter }).countDocuments()) / size);
+
+      return res.status(201).send({ flats, totalPage });
+    }else if (filter == 0 && sort!= 0)  {
+      let flats = await Flat.find()
+        .sort({flat_number:+sort})
+        .skip(skip)
+        .limit(size)
+        .populate("residents")
+        .lean()
+        .exec();
+
+      let totalPage = Math.ceil(
+        (await Flat.find().countDocuments()) / size);
+
+      return res.status(201).send({ flats, totalPage });
+    }
+
+    } catch (e) {
+    return res.status(500).json({ status: "Failed", message: e.message });
+  }
+});
 
 module.exports = router;
--- frontend/src/components/Flat.jsx
+++ frontend/src/components/Flat.jsx
@@ -5,15 +5,32 @@ import { useEffect, useState } from "react";
 export const Flat = () => {
   const [page, setPage] = useState(1);
   const [data, setData] = useState([]);
-  const [limit,setLimit] = useState(0);
+  const [limit, setLimit] = useState(0);
+  const [value, setValue] = useState("0");
+  const [filter, setFilter] = useState("0");
+
+  const handleValue = (e) => {
+    setValue(e.target.value);
+  };
 
   useEffect(() => {
-    getData();
-  }, [page]);
+    if (value.length > 0) {
+      getData();
+    }
+  }, [value, filter, page]);
+
+  const handleFilter = (e) => {
+    setFilter(e.target.value);
+  };
 
   const getData = () => {
+    console.log("Sort:", value);
+    console.log("Sort:", value);
+
     try {
-      fetch(`http://localhost:4500/flat?page=${page}`)
+      fetch(
+        `http://localhost:4500/flat/${filter}/${value}/?page=${page}&size=${4}`
+      )
         .then((d) => d.json())
         .then((res) => {
           setData(res.flats);
@@ -24,86 +41,68 @@ export const Flat = () => {
       console.log("Error:", err);
     }
   };
-
-  const sorting=(value)=>{
-    try {
-      fetch(`http://localhost:4500/flat/sort/${value}`)
-        .then((d) => d.json())
-        .then((res) => {
-          setData(res.flats);
-          setLimit(res.totalPage);
-          console.log(res.flats);
-        });
-    } catch (err) {
-      console.log("Error:", err);
-    }
-  };
-
-
 
   return (
     <div id="flatDivMain">
       <div id="sort_filter_div">
-        <h1 id="sort_filter_div_head">Sort and Filter</h1>
+        <h1 id="sort_filter_div_head">Sort by Flat Number</h1>
         <div id="sortDiv">
-          <select name="" id="sortbyFlatNumber" onChange={sorting}>
-            <option value="">Sort by Flat Number</option>
-            <option value="lowtoHigh">(Low To High)</option>
-            <option value="HighToLow">(High to low)</option>
+          <select name="" id="sortbyFlatNumber" onChange={handleValue}>
+            <option value="0">Flat Number</option>
+            <option value="1">Lower</option>
+            <option value="-1">Upper</option>
           </select>
-         <div id="filterDiv">
-           <div>
-               <input type="checkBox" name="A" />   <label htmlFor="A">A-Block</label> 
-           </div>
-       <div>
-          <input type="checkBox" name="B" />   <label htmlFor="B">B-Block</label>
-       </div>
-         
-        <div>
-        <input type="checkBox" name="C" />   <label htmlFor="C">C-Block</label>  
-        </div>
-         </div>  
-        
+          <div id="filterDiv">
+            <h1 id="sort_filter_div_head">Filter by Resident Type</h1>
+            <select name="" id="filterByType" onChange={handleFilter}>
+              <option value="0">Resident Type</option>
+              <option value="Owner">Owner</option>
+              <option value="Tenant">Tenant</option>
+            </select>
+          </div>
         </div>
       </div>
-<div id="flatList">
-      <table>
-        <tr>
-          <th>Block</th>
-          <th>Flat Number</th>
-          <th>Type</th>
-          <th>No. of Residents</th>
-          <th>Image</th>
-        </tr>
-        {data.map((e, i) => (
-          <tr className="flatData" key={i}>
-            <td>{e.block}</td>
-
-            <td>{e.flat_number}</td>
-            <td>{e.type}</td>
-            <td>{e.residents.length}</td>
-            <Link className="flatListLink" to={`/flat/${e._id}`}>
-              <td>
-                <img className="flatImg" src={e.image} alt="NA" />
-              </td>
-            </Link>
+      <div id="flatList">
+        <table>
+          <tr>
+            <th>Block</th>
+            <th>Flat Number</th>
+            <th>Type</th>
+            <th>No. of Residents</th>
+            <th>Image</th>
           </tr>
-        ))}
-      </table>
-      <div id="btnDiv">
-        <button
-          className="paginationBtn"
-          disabled={page === 1}
-          onClick={() => setPage(page - 1)}
-        >
-          Previous
-        </button>
-        <button className="paginationBtn"  disabled={page === limit}  onClick={() => setPage(page + 1)}>
-          Next
-        </button>
+          {data.map((e, i) => (
+            <tr className="flatData" key={i}>
+              <td>{e.block}</td>
+
+              <td>{e.flat_number}</td>
+              <td>{e.type}</td>
+              <td>{e.residents.length}</td>
+              <Link className="flatListLink" to={`/flat/${e._id}`}>
+                <td>
+                  <img className="flatImg" src={e.image} alt="NA" />
+                </td>
+              </Link>
+            </tr>
+          ))}
+        </table>
+        <div id="btnDiv">
+          <button
+            className="paginationBtn"
+            disabled={page === 1}
+            onClick={() => setPage(page - 1)}
+          >
+            Previous
+          </button>
+          <button
+            className="paginationBtn"
+            disabled={page === limit}
+            onClick={() => setPage(page + 1)}
+          >
+            Next
+          </button>
+        </div>
       </div>
     </div>
-    </div>
-    
   );
 };
